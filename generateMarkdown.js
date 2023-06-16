6ab12bb5741949a20aa6ce0400090aef26d8a3a8
kylechau123 <123038673+kylechau123@users.noreply.github.com>
2023-06-16 13:59:44 -0400

    pushing full code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![](https://img.shields.io/badge/LICENSE-${license}-red)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "MIT") {
+    return "https://opensource.org/license/mit/"
+  } else if (license == "Apache") {
+    return "https://opensource.org/license/apache-2-0/"
+  } else if (license == "Mozilla") {
+    return "https://opensource.org/license/mpl-2-0/"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## License
+  ${license}
+  ${renderLicenseLink(license)}`
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 
-`;
+${renderLicenseBadge(answers.license)}
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contribution
+${answers.contribution}
+
+## Test
+${answers.test}
+
+## Questions
+${answers.github}
+${answers.email}
+
+${renderLicenseSection(answers.license)}`;
 }
 
 module.exports = generateMarkdown;
---
+++ output/output.md
@@ -0,0 +1,26 @@
+# Read Me Generator
+
+![](https://img.shields.io/badge/LICENSE-MIT-red)
+
+## Description
+This project is a read me generator.
+
+## Installation
+Step 1. Access the code from the creator's github. Step 2. Copy the repository into your VS Code to be able to use it. Step 3. Install node packages and you are good to go.
+
+## Usage
+You can use this project to create readme files with ease.
+
+## Contribution
+Contribution to this project would mean that you can create readme files without the need of accessing the terminal at all.
+
+## Test
+This right here is a test actually.
+
+## Questions
+kylechau123
+kylechau123@gmail.com
+
+## License
+  MIT
+  https://opensource.org/license/mit/
